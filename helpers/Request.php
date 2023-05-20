7f898ee8e32632a11d471b215b33185607f81418
elenagarbo <elenagarciabon@gmail.com>
2023-05-20 12:28:37 +0200

    Verify if post has data

--- helpers/Request.php
+++ helpers/Request.php
@@ -1,7 +1,9 @@
 <?php
     class Request {
+        // Verifica si la solicitud es de tipo POST
+        // Verifica que hay datos enviados en el cuerpo de la solicitud (no esta vacio)
         public function isPost() {
-            return $_SERVER['REQUEST_METHOD'] == 'POST';
+            return $_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST);
         }
     }
 ?>

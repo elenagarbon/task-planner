3dc6acdc05cf1120fd3e1506aced186a11e0f52a
elenagarbo <elenagarciabon@gmail.com>
2023-05-20 10:04:43 +0200

    Add title in private zone

--- views/dashboard.php
+++ views/dashboard.php
@@ -6,8 +6,12 @@
     }
 ?>
 <main class="main p-dashboard">
-    <div class="l-content-grid">
-        hola
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h4>Hola <?php echo $_SESSION["user"]["nickname"]?>!</h4>
+            </div>
+        </div>
     </div>
 </main>
 

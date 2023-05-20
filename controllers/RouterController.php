a8ae6569a5aae4f9e1e74aa99492c976ad9614f6
elenagarbo <elenagarciabon@gmail.com>
2023-05-20 10:04:32 +0200

    Add userController in router

--- controllers/RouterController.php
+++ controllers/RouterController.php
@@ -1,14 +1,17 @@
 <?php
+require_once 'controllers/UserController.php';
 require_once 'helpers/Request.php';
 require_once 'helpers/Session.php';
 
 class RouterController {
     private $db;
+    private $userController;
     private $request;
     private $session;
 
     public function __construct($db) {
         $this->db = $db;
+        $this->userController = new UserController($db);
         $this->request = new Request();
         $this->session = new Session();
     }
@@ -20,6 +23,8 @@ class RouterController {
             case 'login':
                 if ($this->session->isLoggedIn()) {
                     header('Location: index.php?action=dashboard');
+                } else if ($this->request->isPost()) {
+                    $this->userController->login();
                 } else {
                     require_once 'views/login.php';
                 }
@@ -27,13 +32,18 @@ class RouterController {
             case 'register':
                 if ($this->session->isLoggedIn()) {
                     header('Location: index.php?action=dashboard');
+                } else if ($this->request->isPost()) {
+                    $this->userController->signUp();
                 } else {
                     require_once 'views/register.php';
                 }
                 break;
+            case 'logout':
+                $this->userController->logout();
+                break;
             case 'dashboard':
                 if ($this->session->isLoggedIn()) {
-                    header('Location: index.php?action=dashboard');
+                    require_once 'views/dashboard.php';
                 } else {
                     header('Location: index.php?action=login');
                 }

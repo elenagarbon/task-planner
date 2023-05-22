d040828cf740e19ac1bcbd8c9b2709560853da3f
elenagarbo <elenagarciabon@gmail.com>
2023-05-22 18:46:01 +0200

    Separete data to use in form selects

---
+++ data/select_options.php
@@ -0,0 +1,34 @@
+<?php
+    $priorities = array(
+        "high" => "Alta",
+        "medium" => "Media",
+        "low" => "Baja"
+    );
+
+    $taskTypes = array(
+        "work" => array(
+            "name" => "Trabajo",
+            "image" => "resources/images/work.png"
+        ),
+        "student" => array(
+            "name" => "Estudio",
+            "image" => "resources/images/study.png"
+        ),
+        "house" => array(
+            "name" => "Casa",
+            "image" => "resources/images/house.png"
+        ),
+        "birthday" => array(
+            "name" => "Cumpleaños",
+            "image" => "resources/images/birthday.png"
+        ),
+        "payment" => array(
+            "name" => "Pagos",
+            "image" => "resources/images/payment.png"
+        ),
+        "other" => array(
+            "name" => "Otros",
+            "image" => "resources/images/other.png"
+        ),
+    );
+?>

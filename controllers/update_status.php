1c218178ae578b761fd164cf6e319c442a9cda2c
elenagarbo <elenagarciabon@gmail.com>
2023-05-29 09:40:37 +0200

    Remove echo

--- controllers/update_status.php
+++ controllers/update_status.php
@@ -2,13 +2,8 @@
     // Recuperar los datos de la solicitud
     $json_data = file_get_contents('php://input');
     $data = json_decode($json_data, true);
-    var_dump($json_data);
-    var_dump($data);
-
     $task_id = $data['task_id'];
     $status = $data['status'];
-    echo $task_id;
-    echo $status;
 
     // Realizar la actualización en la base de datos
     try {
@@ -21,7 +16,7 @@
         $stmt->bindParam(':id_task', $task_id);
         $stmt->execute();
 
-        echo "Actualización exitosa";
+        echo "Actualizado estado";
     } catch (PDOException $e) {
         echo "Error al actualizar el estado de la tarea: " . $e->getMessage();
     }
